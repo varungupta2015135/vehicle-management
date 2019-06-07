d4650baa2c7c9331d05b71cb9932a85e6d68c6c5
Gupta <Varun.Gupta3@Honeywell.com>
2019-06-07 15:13:46 +0530

    Completed home page after login/register

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var firebase = require("firebase");
 
+app.use(express.static(__dirname + '/public'));
+
 // Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyApsFPxfBl6mNEjv6LPpDAuxinaYZQkmNE",
@@ -24,6 +26,10 @@ app.get("/", function(req, res) {
   //     .ref("/TestMessages")
   //     .set({ TestMessage: "GET REQUEST" });
 });
+
+app.get("/home", function(req, res){
+    res.render("home.ejs");
+})
 
 var server = app.listen(8000, function() {
   console.log("Server running at port 8000");
