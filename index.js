3523e09bc878f16fb78c605e6c58de9770e787ff
Gupta <Varun.Gupta3@Honeywell.com>
2019-06-07 11:48:00 +0530

    Hooked up firebase

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var firebase = require("firebase");
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyApsFPxfBl6mNEjv6LPpDAuxinaYZQkmNE",
+  authDomain: "vehicle-management-243006.firebaseapp.com",
+  databaseURL: "https://vehicle-management-243006.firebaseio.com",
+  projectId: "vehicle-management-243006",
+  storageBucket: "",
+  messagingSenderId: "703497089543",
+  appId: "1:703497089543:web:bcbd425566e424c3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+app.get("/", function(req, res) {
+  console.log("Home Page!");
+  res.render("index.ejs");
+  //insert key:value
+  //   firebase
+  //     .database()
+  //     .ref("/TestMessages")
+  //     .set({ TestMessage: "GET REQUEST" });
+});
+
+var server = app.listen(8000, function() {
+  console.log("Server running at port 8000");
+});
